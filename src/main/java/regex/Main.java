85545792032acfda7f1648ae4c8971b4ab3b9d34
ANIme-A <Tymoncui@gmail.com>
2025-11-29 00:25:45 -0500

    1

--- src/main/java/regex/Main.java
+++ src/main/java/regex/Main.java
@@ -38,7 +38,9 @@ public class Main {
      * @return whether the string satisfies the password requirements
      */
     public static boolean checkForPassword(String str, int minLength) {
-        final boolean propertyOne = Pattern.matches("REPLACE WITH CORRECT REGEX", str);
+
+        String regex = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{" + minLength + ",}";
+        final boolean propertyOne = Pattern.matches(regex, str);
         // as needed, modify this code.
         return propertyOne;
     }
@@ -55,7 +57,9 @@ public class Main {
      * @return a list containing the email addresses in the string.
      */
     public static List<String> extractEmails(String str) {
-        final Pattern pattern = Pattern.compile("REPLACE WITH CORRECT REGEX");
+        String regex = "[A-Za-z0-9._-]+@(mail.)?utoronto.ca";
+
+        final Pattern pattern = Pattern.compile(regex);
         final Matcher matcher = pattern.matcher(str);
         final List<String> result = new ArrayList<>();
         while (matcher.find()) {
@@ -76,6 +80,7 @@ public class Main {
      * @return whether str contains the same capital letter twice.
      */
     public static boolean checkForDoubles(String str) {
-        return str.matches("replace with correct regex");
+        String regex = ".*([A-Z]).*\1.*";
+        return str.matches(regex);
     }
 }
